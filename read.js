169c6223173826d717b76f48f79edb8ff9efcea1
greggry <greggryw@protonmail.com>
2021-12-30 20:06:40 +0100

    reorganized code

--- read.js
+++ read.js
@@ -5,16 +5,16 @@ const Ask = require('@greggry/ask');
 
 const ask = new Ask();
 
+const wrapInQuotes = string => `"${string}"`;
+
 const main = async () => {
   let input = await ask.ask("(pass in 'q' to exit) Text to read: ");
 
   while (input?.toLowerCase() !== 'q') {
-    if (input !== undefined) {
-      console.log(
-        `processing ${input.length <= 50 ? "'" + input + "'" : 'the input'}...`
-      );
-      exec(`echo ${input} | festival --tts`);
-    }
+    console.log(
+      `processing ${input.length <= 50 ? wrapInQuotes(input) : 'the input'}...`
+    );
+    exec(`echo ${input} | festival --tts`);
 
     input = await ask.ask('Text to read: ');
   }
